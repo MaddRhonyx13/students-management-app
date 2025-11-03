b7d51e768cc30e8745ba343ae2ca9c7a1aa9dbff
Tumisang Malataliana <tummiebabbie6@gmail.com>
2025-11-03 14:23:56 +0200

    ...

--- frontend/src/services/studentService.js
+++ frontend/src/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://students-management-app-production-548b.up.railway.app/';
+const API_BASE_URL = 'https://students-management-app-production-548b.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
